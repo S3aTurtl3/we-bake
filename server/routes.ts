afd50699c16331c344b228bcfb608a6817185957
Ore <orealao@mit.edu>
2023-10-03 03:49:11 -0400

    declare some subset of api endpoints

--- server/routes.ts
+++ server/routes.ts
@@ -1,12 +1,12 @@
 import { ObjectId } from "mongodb";
-
-import { Router, getExpressRouter } from "./framework/router";
-
-import { Friend, Post, User, WebSession } from "./app";
-import { PostDoc, PostOptions } from "./concepts/post";
+import { Friend, User, WebSession } from "./app";
+import { ManuallyEnteredRecipe } from "./concepts/recipe";
 import { UserDoc } from "./concepts/user";
 import { WebSessionDoc } from "./concepts/websession";
+import { Router, getExpressRouter } from "./framework/router";
 import Responses from "./responses";
+
+// write the specifications for all the sync (also make sfb)
 
 class Routes {
   @Router.get("/session")
@@ -57,37 +57,65 @@ class Routes {
     return { msg: "Logged out!" };
   }
 
-  @Router.get("/posts")
-  async getPosts(author?: string) {
-    let posts;
-    if (author) {
-      const id = (await User.getUserByUsername(author))._id;
-      posts = await Post.getByAuthor(id);
-    } else {
-      posts = await Post.getPosts({});
-    }
-    return Responses.posts(posts);
+  /**
+   *
+   * @param session
+   * @param recipe
+   * @returns object containing id of recipe
+   */
+  @Router.post("/recipes")
+  async createRecipe(session: WebSessionDoc, recipe: ManuallyEnteredRecipe) {
+    const user = WebSession.getUser(session);
   }
 
-  @Router.post("/posts")
-  async createPost(session: WebSessionDoc, content: string, options?: PostOptions) {
+  @Router.patch("/recipes/:_id")
+  async updateRecipe(session: WebSessionDoc, _id: ObjectId, update: Partial<ManuallyEnteredRecipe>) {
     const user = WebSession.getUser(session);
-    const created = await Post.create(user, content, options);
-    return { msg: created.msg, post: await Responses.post(created.post) };
   }
 
-  @Router.patch("/posts/:_id")
-  async updatePost(session: WebSessionDoc, _id: ObjectId, update: Partial<PostDoc>) {
+  /**
+   *
+   * @param session
+   * @param _id the id of the collection to which a recipe should be added
+   * @param recipeId the id of the recipe to add to the collection
+   */
+  @Router.post("/recipe_collections/:_id/recipes")
+  async addRecipeToCollection(session: WebSessionDoc, _id: ObjectId, recipeId: ObjectId) {
     const user = WebSession.getUser(session);
-    await Post.isAuthor(user, _id);
-    return await Post.update(_id, update);
   }
 
-  @Router.delete("/posts/:_id")
-  async deletePost(session: WebSessionDoc, _id: ObjectId) {
+  /**
+   *
+   * @param session
+   * @param _id the id of the collection
+   * @param recipeId the id of the recipe to remove from the collection
+   */
+  @Router.delete("/recipe_collections/:_id/recipes/:recipeId")
+  async removeRecipe(session: WebSessionDoc, _id: ObjectId, recipeId: ObjectId) {
     const user = WebSession.getUser(session);
-    await Post.isAuthor(user, _id);
-    return Post.delete(_id);
+  }
+
+  /**
+   *
+   * @param session
+   * @param _id the id of the RecipeCollection
+   * @returns recipes existing in the collection
+   */
+  @Router.get("/recipe_collections/:_id")
+  async getRecipesFromCollection(session: WebSessionDoc, _id: ObjectId) {
+    const user = WebSession.getUser(session);
+  }
+
+  /**
+   *
+   * @param session
+   * @param _id the id of the recipe's access control
+   * @param userId the id of the user who will be granted access to the recipe
+   */
+  @Router.patch("/recipe_access_controls/:_id")
+  async grantUserAccessToRecipe(session: WebSessionDoc, _id: ObjectId, userId: ObjectId) {
+    const user = WebSession.getUser(session);
+    return user;
   }
 
   @Router.get("/friends")
